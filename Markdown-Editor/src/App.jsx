aa9cbbe02d118baf140a1b12a171261572c188ed
gioshwua <reanokennethjoshua11@gmail.com>
2023-01-11 23:48:18 +0800

    altered input placeholder

--- Markdown-Editor/src/App.jsx
+++ Markdown-Editor/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <div className="content">
         <Input
           input={inputtedSomething}
-          placeholder="TYPE YOUR MARKDOWN HERE ->"
+          placeholder="TYPE YOUR MARKDOWN HERE "
           value={markdown}
         />
         <Output output={markdown} />
